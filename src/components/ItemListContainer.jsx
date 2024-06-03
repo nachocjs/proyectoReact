647866a949bdf28f99190da8d3e368f876b6c18c
IgnacioRamos <iramos99106@gmail.com>
2024-06-03 17:35:56 -0300

    actualizo

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import data from "../data/productos.json";
 
 export const ItemListContainer = () => {
+  
+  let [productos, setProductos] = useState([]);
+
+  const pedirProductos = () => {
+    return new Promise((resolve,reject) => {
+      setTimeout(() => {
+        resolve(data);
+      }, 1000);
+    })
+  }
+
+  pedirProductos()
+    .then((res) => {
+      setProductos(res);
+   })
+
   return (
-    <div className='mian-container'>
-        <img className='manteniniento-img' src="./src/assets/mantenimiento.png" alt="" />
+    <div className="productos-container">
     </div>
   )
 }
