138c977a68ac467d36ca0ad5c1a834f0e94bf4d0
IgnacioRamos <iramos99106@gmail.com>
2024-06-28 16:23:53 -0300

    final correccion de img

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 export const Logo = () => {
   return (
     <div className='logo-container'>
-        <NavLink to="/"><img className='logo-img' src="/src/assets/kisspng-laptop-computer-icons-computer-monitors-5b3c00772c2241.4970797915306589351808 (1).png" alt="Logo" /></NavLink>
+        <NavLink to="/"><img className='logo-img' src="./assets/kisspng-laptop-computer-icons-computer-monitors-5b3c00772c2241.4970797915306589351808 (1).png" alt="Logo" /></NavLink>
         <NavLink to="/" className='logo-link'>Mr Componentes</NavLink>
     </div>
   )
